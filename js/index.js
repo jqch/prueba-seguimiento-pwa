dc22e613a5f4ed69c3d5c7519aa40ea2c02dad78
Jhonny Quispe <jhonnyq17@gmail.com>
2021-07-26 10:37:39 -0400

    se ajustaton algunos puntos para mejorar el rendimiento

--- js/index.js
+++ js/index.js
@@ -342,58 +342,60 @@ var vm = new Vue({
 					}
 				});
 
+				chart.tapToActivate = false
+
 			});
 		},
 
-		barchart(div, datos) {
-			am4core.addLicense("ch-custom-attribution");
-			am4core.ready(function () {
+		// barchart(div, datos) {
+		// 	am4core.addLicense("ch-custom-attribution");
+		// 	am4core.ready(function () {
 
-				// Themes begin
-				am4core.useTheme(am4themes_animated);
-				// Themes end
+		// 		// Themes begin
+		// 		am4core.useTheme(am4themes_animated);
+		// 		// Themes end
 
-				// Create chart instance
-				var chart = am4core.create(div, am4charts.XYChart3D);
-				chart.responsive.enabled = true;
+		// 		// Create chart instance
+		// 		var chart = am4core.create(div, am4charts.XYChart3D);
+		// 		chart.responsive.enabled = true;
 
-				chart.data = datos
+		// 		chart.data = datos
 
-				// Create axes
-				var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-				categoryAxis.dataFields.category = "nombre";
-				categoryAxis.renderer.grid.template.location = 0;
-				categoryAxis.renderer.minGridDistance = 30;
-				categoryAxis.renderer.labels.template.horizontalCenter = "middle";
-				categoryAxis.renderer.labels.template.rotation = 270;
-				categoryAxis.renderer.labels.template.hideOversized = false;
-				categoryAxis.renderer.labels.template.truncate = true;
-				categoryAxis.renderer.labels.template.maxWidth = 110;
+		// 		// Create axes
+		// 		var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+		// 		categoryAxis.dataFields.category = "nombre";
+		// 		categoryAxis.renderer.grid.template.location = 0;
+		// 		categoryAxis.renderer.minGridDistance = 30;
+		// 		categoryAxis.renderer.labels.template.horizontalCenter = "middle";
+		// 		categoryAxis.renderer.labels.template.rotation = 270;
+		// 		categoryAxis.renderer.labels.template.hideOversized = false;
+		// 		categoryAxis.renderer.labels.template.truncate = true;
+		// 		categoryAxis.renderer.labels.template.maxWidth = 110;
 
-				var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-				valueAxis.title.text = "Monto en Bs.";
-				valueAxis.renderer.labels.template.adapter.add("text", function (text) {
-					return text;
-				});
+		// 		var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+		// 		valueAxis.title.text = "Monto en Bs.";
+		// 		valueAxis.renderer.labels.template.adapter.add("text", function (text) {
+		// 			return text;
+		// 		});
 
-				// Create series
-				var series = chart.series.push(new am4charts.ColumnSeries3D());
-				series.dataFields.valueY = "devengado";
-				series.dataFields.categoryX = "nombre";
-				series.name = "Monto devengado";
-				series.clustered = false;
-				series.columns.template.tooltipText = "Devengado: [bold]{valueY}[/]";
-				series.columns.template.fillOpacity = 0.9;
+		// 		// Create series
+		// 		var series = chart.series.push(new am4charts.ColumnSeries3D());
+		// 		series.dataFields.valueY = "devengado";
+		// 		series.dataFields.categoryX = "nombre";
+		// 		series.name = "Monto devengado";
+		// 		series.clustered = false;
+		// 		series.columns.template.tooltipText = "Devengado: [bold]{valueY}[/]";
+		// 		series.columns.template.fillOpacity = 0.9;
 
-				var series2 = chart.series.push(new am4charts.ColumnSeries3D());
-				series2.dataFields.valueY = "vigente";
-				series2.dataFields.categoryX = "nombre";
-				series2.name = "Monto vigente";
-				series2.clustered = false;
-				series2.columns.template.tooltipText = "Vigente: [bold]{valueY}[/]";
+		// 		var series2 = chart.series.push(new am4charts.ColumnSeries3D());
+		// 		series2.dataFields.valueY = "vigente";
+		// 		series2.dataFields.categoryX = "nombre";
+		// 		series2.name = "Monto vigente";
+		// 		series2.clustered = false;
+		// 		series2.columns.template.tooltipText = "Vigente: [bold]{valueY}[/]";
 
-			}); // end am4core.ready()
-		},
+		// 	}); // end am4core.ready()
+		// },
 		piechart(div, datos) {
 
 			am4core.ready(function () {
@@ -524,66 +526,6 @@ var vm = new Vue({
 
 				am4core.addLicense("ch-custom-attribution");
 
-				// am4core.ready(function() {
-				//   // Themes begin
-				//   am4core.useTheme(am4themes_animated);
-				//   // Themes end
-
-				//   // Create chart instance
-				//   var chart = am4core.create("chartdiv", am4charts.XYChart);
-				//   chart.scrollbarX = new am4core.Scrollbar();
-
-				//   // Add data
-				//   chart.data = response.data.entidad;
-
-				//   // Create axes
-				//   var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-				//   categoryAxis.dataFields.category = "entidad";
-				//   categoryAxis.renderer.grid.template.location = 0;
-				//   categoryAxis.renderer.minGridDistance = 30;
-				//   categoryAxis.renderer.labels.template.horizontalCenter = "right";
-				//   categoryAxis.renderer.labels.template.verticalCenter = "middle";
-				//   categoryAxis.renderer.labels.template.rotation = 270;
-				// 	categoryAxis.renderer.labels.template.hideOversized = false;
-				// 	categoryAxis.renderer.labels.template.truncate = true;
-				// 	categoryAxis.renderer.labels.template.maxWidth = 110;
-				//   categoryAxis.tooltip.disabled = true;
-				//   categoryAxis.renderer.minHeight = 110;
-
-				//   var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-				//   valueAxis.renderer.minWidth = 50;
-				//   valueAxis.title.text = "Cantidad";
-				//   valueAxis.title.fontWeight = "bold";
-
-				//   // Create series
-				//   var series = chart.series.push(new am4charts.ColumnSeries());
-				//   series.sequencedInterpolation = true;
-				//   series.dataFields.valueY = "cant";
-				//   series.dataFields.categoryX = "entidad";
-				//   series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
-				//   series.columns.template.strokeWidth = 0;
-
-				//   series.tooltip.pointerOrientation = "vertical";
-
-				//   series.columns.template.column.cornerRadiusTopLeft = 10;
-				//   series.columns.template.column.cornerRadiusTopRight = 10;
-				//   series.columns.template.column.fillOpacity = 0.8;
-
-				//   // on hover, make corner radiuses bigger
-				//   var hoverState = series.columns.template.column.states.create("hover");
-				//   hoverState.properties.cornerRadiusTopLeft = 0;
-				//   hoverState.properties.cornerRadiusTopRight = 0;
-				//   hoverState.properties.fillOpacity = 1;
-
-				//   series.columns.template.adapter.add("fill", function(fill, target) {
-				//     return chart.colors.getIndex(target.dataItem.index);
-				//   });
-
-				//   // Cursor
-				//   chart.cursor = new am4charts.XYCursor();
-
-				// }); // end am4core.ready()
-
 				am4core.ready(function () {
 
 					// Themes begin
@@ -702,163 +644,7 @@ var vm = new Vue({
 					chart.cursor.lineY.strokeOpacity = 0;
 
 				}); // end am4core.ready()
-
-				am4core.ready(function () {
-
-					// Themes begin
-					am4core.useTheme(am4themes_animated);
-					// Themes end
-
-					// Create chart instance
-					var chart = am4core.create("chartdiv2", am4charts.XYChart3D);
-					chart.paddingBottom = 30;
-					chart.angle = 35;
-
-					// Add data
-					chart.data = [{
-						"country": "USA",
-						"visits": 4025
-					}, {
-						"country": "China",
-						"visits": 1882
-					}, {
-						"country": "Japan",
-						"visits": 1809
-					}, {
-						"country": "Germany",
-						"visits": 1322
-					}, {
-						"country": "UK",
-						"visits": 1122
-					}, {
-						"country": "France",
-						"visits": 1114
-					}, {
-						"country": "India",
-						"visits": 984
-					}, {
-						"country": "Spain",
-						"visits": 711
-					}, {
-						"country": "Netherlands",
-						"visits": 665
-					}, {
-						"country": "Russia",
-						"visits": 580
-					}, {
-						"country": "South Korea",
-						"visits": 443
-					}, {
-						"country": "Canada",
-						"visits": 441
-					}, {
-						"country": "Brazil",
-						"visits": 395
-					}, {
-						"country": "Italy",
-						"visits": 386
-					}, {
-						"country": "Taiwan",
-						"visits": 338
-					}];
-
-					// Create axes
-					var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-					categoryAxis.dataFields.category = "country";
-					categoryAxis.renderer.grid.template.location = 0;
-					categoryAxis.renderer.minGridDistance = 20;
-					categoryAxis.renderer.inside = true;
-					categoryAxis.renderer.grid.template.disabled = true;
-
-					let labelTemplate = categoryAxis.renderer.labels.template;
-					labelTemplate.rotation = -90;
-					labelTemplate.horizontalCenter = "left";
-					labelTemplate.verticalCenter = "middle";
-					labelTemplate.dy = 10; // moves it a bit down;
-					labelTemplate.inside = false; // this is done to avoid settings which are not suitable when label is rotated
-
-					var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-					valueAxis.renderer.grid.template.disabled = true;
-
-					// Create series
-					var series = chart.series.push(new am4charts.ConeSeries());
-					series.dataFields.valueY = "visits";
-					series.dataFields.categoryX = "country";
-
-					var columnTemplate = series.columns.template;
-					columnTemplate.adapter.add("fill", function (fill, target) {
-						return chart.colors.getIndex(target.dataItem.index);
-					})
-
-					columnTemplate.adapter.add("stroke", function (stroke, target) {
-						return chart.colors.getIndex(target.dataItem.index);
-					})
-
-				}); // end am4core.ready()
-
-				// am4core.ready(function() {
-				//   // Themes begin
-				//   am4core.useTheme(am4themes_animated);
-				//   // Themes end
-
-				//   var chart = am4core.create("chartdiv2", am4charts.XYChart);
-
-				//   chart.data = response.data.sector;
-
-				//   chart.padding(40, 40, 40, 40);
-
-				//   var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-				//   categoryAxis.dataFields.category = "sector";
-				//   categoryAxis.renderer.labels.template.rotation = 270;
-				//   categoryAxis.renderer.labels.template.hideOversized = false;
-				//   categoryAxis.renderer.labels.template.truncate = true;
-				//   categoryAxis.renderer.labels.template.maxWidth = 120;
-				//   //categoryAxis.renderer.labels.template.wrap = true;
-				//   categoryAxis.renderer.minGridDistance = 20;
-				//   categoryAxis.renderer.labels.template.horizontalCenter = "right";
-				//   categoryAxis.renderer.labels.template.verticalCenter = "middle";
-				//   categoryAxis.tooltip.label.rotation = 270;
-				//   categoryAxis.tooltip.label.horizontalCenter = "right";
-				//   categoryAxis.tooltip.label.verticalCenter = "middle";
-
-				//   var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-				//   valueAxis.min = 0;
-				//   valueAxis.extraMax = 0.1;
-				//   //valueAxis.rangeChangeEasing = am4core.ease.linear;
-				//   //valueAxis.rangeChangeDuration = 1500;
-
-				//   var series = chart.series.push(new am4charts.ColumnSeries());
-				//   series.dataFields.categoryX = "sector";
-				//   series.dataFields.valueY = "cant";
-				//   series.tooltipText = "{valueY.value}"
-				//   series.columns.template.strokeOpacity = 0;
-				//   series.columns.template.column.cornerRadiusTopRight = 10;
-				//   series.columns.template.column.cornerRadiusTopLeft = 10;
-				//   //series.interpolationDuration = 1500;
-				//   //series.interpolationEasing = am4core.ease.linear;
-				//   var labelBullet = series.bullets.push(new am4charts.LabelBullet());
-				//   labelBullet.label.verticalCenter = "bottom";
-				//   labelBullet.label.dy = -10;
-				//   labelBullet.label.text = "{values.valueY.workingValue.formatNumber('#.')}";
-
-				//   chart.zoomOutButton.disabled = true;
-
-				//   // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-				//   series.columns.template.adapter.add("fill", function (fill, target) {
-				//   return chart.colors.getIndex(target.dataItem.index);
-				//   });
-
-				//   setInterval(function () {
-				//   am4core.array.each(chart.data, function (item) {
-				//     item.visits += Math.round(Math.random() * 200 - 100);
-				//     item.visits = Math.abs(item.visits);
-				//   })
-				//   chart.invalidateRawData();
-				//   }, 2000)
-
-				//   categoryAxis.sortBySeries = series;
-				// }); // end am4core.ready()
-
+				
 				this.loading = false
 			}
 
